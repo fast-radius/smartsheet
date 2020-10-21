0dcae136c71a1373d0378833761712e3b67e57e2
Tam Dang <teedang19@gmail.com>
2020-10-21 17:01:39 -0500

    Add delete_rows functionality
    Also:
    1. Ups the response timeout via a config variable
    2. Refactors some of the duplication in the client

--- lib/smartsheet/client_behaviour.ex
+++ lib/smartsheet/client_behaviour.ex
@@ -1,34 +1,11 @@
 defmodule Smartsheet.ClientBehaviour do
-  @callback get_sheet(String.t(), List) ::
-              {:ok, Smartsheet.Response.t(), Smartsheet.Sheet.t()}
-              | {:error, Smartsheet.Response.t()}
-
-  @callback create_sheet(map()) ::
-              {:ok, Smartsheet.Response.t(), Smartsheet.Sheet.t()}
-              | {:error, Smartsheet.Response.t()}
-              | {:internal_error, atom()}
-  @callback get_columns(String.t(), List) ::
-              {:ok, Smartsheet.Response.t(), [Smartsheet.Column]}
-              | {:error, Smartsheet.Response.t()}
-              | {:internal_error, atom()}
-
-  @callback add_rows(String.t(), any) ::
-              {:ok, Smartsheet.Response.t(), [Smartsheet.Row.t()]}
-              | {:error, Smartsheet.Response.t()}
-              | {:internal_error, atom()}
-
-  @callback update_rows(String.t(), any) ::
-              {:ok, Smartsheet.Response.t(), [Smartsheet.Row.t()]}
-              | {:error, Smartsheet.Response.t()}
-              | {:internal_error, atom()}
-
-  @callback get_row(String.t(), String.t()) ::
-              {:ok, Smartsheet.Response.t(), Smartsheet.Row.t()}
-              | {:error, Smartsheet.Response.t()}
-              | {:internal_error, atom()}
-
   @callback add_webhook(map()) ::
               {:ok, Smartsheet.Response.t(), Smartsheet.Webhook.t()}
+              | {:error, Smartsheet.Response.t()}
+              | {:internal_error, atom()}
+
+  @callback list_webhooks() ::
+              {:ok, Smartsheet.Response.t(), [Smartsheet.Webhook.t()]}
               | {:error, Smartsheet.Response.t()}
               | {:internal_error, atom()}
 
@@ -42,8 +19,37 @@ defmodule Smartsheet.ClientBehaviour do
               | {:error, Smartsheet.Response.t()}
               | {:internal_error, atom()}
 
-  @callback list_webhooks() ::
-              {:ok, Smartsheet.Response.t(), [Smartsheet.Webhook.t()]}
+  @callback create_sheet(map()) ::
+              {:ok, Smartsheet.Response.t(), Smartsheet.Sheet.t()}
+              | {:error, Smartsheet.Response.t()}
+              | {:internal_error, atom()}
+
+  @callback get_sheet(String.t(), List) ::
+              {:ok, Smartsheet.Response.t(), Smartsheet.Sheet.t()}
+              | {:error, Smartsheet.Response.t()}
+
+  @callback get_columns(String.t(), List) ::
+              {:ok, Smartsheet.Response.t(), [Smartsheet.Column]}
+              | {:error, Smartsheet.Response.t()}
+              | {:internal_error, atom()}
+
+  @callback add_rows(String.t(), any) ::
+              {:ok, Smartsheet.Response.t(), [Smartsheet.Row.t()]}
+              | {:error, Smartsheet.Response.t()}
+              | {:internal_error, atom()}
+
+  @callback get_row(String.t(), String.t()) ::
+              {:ok, Smartsheet.Response.t(), Smartsheet.Row.t()}
+              | {:error, Smartsheet.Response.t()}
+              | {:internal_error, atom()}
+
+  @callback update_rows(String.t(), any) ::
+              {:ok, Smartsheet.Response.t(), [Smartsheet.Row.t()]}
+              | {:error, Smartsheet.Response.t()}
+              | {:internal_error, atom()}
+
+  @callback delete_rows(String.t(), List) ::
+              {:ok, Smartsheet.Response.t(), map()}
               | {:error, Smartsheet.Response.t()}
               | {:internal_error, atom()}
 end
